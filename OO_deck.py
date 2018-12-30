7a2fc364f623a723ab58dca87f3e79358a5dc5e3
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-12-30 14:02:22 +0100

    Add files via upload

--- OO_deck.py
+++ OO_deck.py
@@ -24,3 +24,15 @@ deck = FrenchDeck()
 print(len(deck))
 # Show use of getitem on deck.
 print(deck[-1])
+
+# Sort the deck.
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+for card in sorted(deck, key=spades_high):
+    print(card)
